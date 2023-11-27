8afc3229ac8ccfa29d5aa626eafeed8dd6045ce5
Asad1021 <104274350+Asad1021@users.noreply.github.com>
2023-11-27 20:27:31 +0530

    all functions updated

---
+++ functioncall.sql
@@ -0,0 +1,27 @@
+SELECT InsertPatient(
+    16, -- p_id
+    'Male', -- p_gender
+    'John Doe', -- p_name
+    '123 Example St', -- p_address
+    30 -- p_age
+);
+
+
+SELECT InsertHistory(
+    3, -- h_id
+    16, -- p_id
+    '2023-11-27', -- date
+    'Fine', -- p_condition
+    'Brain Surgery', -- p_surgery
+    'www.google.com' -- p_link
+);
+
+
+SELECT InsertDoctor(
+	3333,'asad',22);
+    
+SELECT InsertSchedule(
+    3, -- s_id
+    '10:00:00', -- Start_Time
+    '11:00:00' -- End_Time
+);
--- functions.sql
+++ functions.sql
@@ -13,3 +13,93 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+--funtion for inserting data into the "patient" table
+CREATE FUNCTION InsertPatient (
+    p_id INT,
+    p_gender VARCHAR(10),
+    p_name VARCHAR(255),
+    p_address VARCHAR(255),
+    p_age INT
+)
+RETURNS INT
+AS $$
+DECLARE
+    num_patients INT;
+BEGIN
+
+        -- Insert into the patient table
+        INSERT INTO patient (p_id, p_gender, p_name, p_address, p_age)
+		VALUES (p_id, p_gender, p_name, p_address, p_age);
+   		select count(*) into num_patients from patient;
+    RETURN num_patients;
+END;
+$$ LANGUAGE plpgsql;
+
+
+--function for inserting data into the "medical_history" table
+CREATE FUNCTION InsertHistory (
+  h_id INT ,
+  p_id INT ,
+  date DATE,
+  p_condition VARCHAR(255),
+  p_surgery VARCHAR(255),
+  p_link VARCHAR(255)
+)
+RETURNS INT
+AS $$
+DECLARE
+    num_his INT;
+BEGIN
+
+        -- Insert into the history table
+        INSERT INTO  Medical_History(h_id, p_id,date, p_condition, p_surgery, p_link)
+		VALUES (h_id, p_id,date, p_condition, p_surgery, p_link);
+		   		select count(*) into num_his  from medical_history;
+    RETURN num_his ;
+END;
+$$ LANGUAGE plpgsql;
+
+
+--funtion for inserting data into the "doctors" table
+CREATE FUNCTION InsertDoctor (
+  d_reg_id INT ,
+  d_name VARCHAR(255) ,
+  d_cabin_no INT 
+)
+RETURNS INT
+AS $$
+DECLARE
+    num_doctors INT;
+BEGIN
+
+        -- Insert into the doctor table
+        INSERT INTO doctors(d_reg_id,d_name,d_cabin_no)
+		VALUES (d_reg_id,d_name,d_cabin_no);
+   		select count(*) into num_doctors from doctors;
+    RETURN num_doctors;
+END;
+$$ LANGUAGE plpgsql;
+
+
+--funtion for inserting data into the "schedule" table
+CREATE FUNCTION InsertSchedule (
+  s_id INT ,
+  Start_Time TIME ,
+  End_Time TIME 
+)
+RETURNS INT
+AS $$
+DECLARE
+    num_schedules INT;
+BEGIN
+
+        -- Insert into the schedules table
+        INSERT INTO schedule(s_id,start_time,end_time)
+		VALUES (s_id,start_time,end_time);
+   		select count(*) into num_schedules from schedule;
+    RETURN  num_schedules;
+END;
+$$ LANGUAGE plpgsql;
+
+
